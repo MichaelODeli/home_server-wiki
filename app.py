8f6cb9de7e5021c9627eca53a058cecbf796f16a
MichaelODeli <daniilrew@gmail.com>
2023-12-03 13:53:30 +0500

    add user ip logging

--- app.py
+++ app.py
@@ -200,6 +200,5 @@ def toggle_navbar_collapse(n, is_open):
         return not is_open
     return is_open
 
-
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run_server(debug=True, host="0.0.0.0", port=80)
--- pages/home.py
+++ pages/home.py
@@ -11,11 +11,16 @@ from dash import (
     no_update,
 )
 import dash_mantine_components as dmc
+from flask import request
 
 register_page(__name__, path="/", icon="fa-solid:home")
 
-layout = dmc.Container(
-    children=[],
-    pt=20,
-    style={"paddingTop": 20},
-)
+def layout():
+    lay = dmc.Container(
+        children=[],
+        pt=20,
+        style={"paddingTop": 20},
+    )
+    print('client ip', request.remote_addr)
+    print('server link', request.base_url)
+    return lay
