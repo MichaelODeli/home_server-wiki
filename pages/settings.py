6b887f43297f8294d4e78e5f6e6723aafd1345a8
MichaelODeli <daniilrew@gmail.com>
2023-12-05 19:28:13 +0500

    Settings init page

--- pages/settings.py
+++ pages/settings.py
@@ -14,8 +14,29 @@ import dash_mantine_components as dmc
 
 register_page(__name__, path="/settings", icon="fa-solid:home")
 
-layout = dmc.Container(
-    children=[],
-    pt=20,
-    style={"paddingTop": 20},
-)
+def layout(): 
+    return dmc.Container(
+        children=[
+            dmc.Space(h=10),
+            dmc.Tabs(
+                [
+                    dmc.TabsList(
+                        [
+                            dmc.Tab("Основные настройки", value="main"),
+                            dmc.Tab("Настройки сервера", value="server"),
+                            dmc.Tab("Обработчик файлов", value="files"),
+                        ],
+                        grow=True
+                    ),
+                    dmc.TabsPanel(children=["Tab content"], value="main"),
+                    dmc.TabsPanel(children=["Tab content"], value="server"),
+                    dmc.TabsPanel(children=["Tab content"], value="files"),
+                ],
+                variant="outline",
+                orientation="vertical",
+                className='block-background'
+            )
+        ],
+        pt=20,
+        style={"paddingTop": 20},
+    )
