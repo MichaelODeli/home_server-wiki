39f3fd3d9b71fa7c1f9638643def61d4006581ab
MichaelODeli <daniilrew@gmail.com>
2024-03-15 17:34:12 +0500

    audioplayer left column redesign
    - buttons with icons
    - affix with song name and volume control
    - autohide collapse button for phone

---
+++ controllers/cont_audioplayer.py
@@ -0,0 +1,167 @@
+import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
+from dash_iconify import DashIconify
+from dash import dcc, html
+
+
+def create_table(df):
+    """
+    Вывод таблицы на основе Pandas.df
+    """
+    columns, values = df.columns, df.values
+    header = [html.Tr([html.Th(col) for col in columns])]
+    rows = [html.Tr([html.Td(cell) for cell in row]) for row in values]
+    table = [html.Thead(header), html.Tbody(rows)]
+    return table
+
+
+def get_buttongroup_with_icons(lst):
+    """
+    Вывод вертикальной группы с кнопками и иконками.
+    Формат вложенного списка: [[button_text, button_icon_name, button_id], ...]
+    """
+    buttons_list = []
+    for element in lst:
+        button_text = element[0]
+        button_icon_name = element[1]
+        button_id = element[2]
+        if button_icon_name == "playlist":
+            icon_name = "material-symbols:playlist-play"
+        elif button_icon_name == "genre":
+            icon_name = "material-symbols:genres"
+        elif button_icon_name == "artist":
+            icon_name = "material-symbols:artist"
+        elif button_icon_name == "home":
+            icon_name = "material-symbols:home"
+        elif button_icon_name == "search":
+            icon_name = "material-symbols:search"
+        elif button_icon_name == "catalogs":
+            icon_name = "material-symbols:folder-open"
+        else:
+            icon_name = None
+
+        icon = (
+            DashIconify(icon=icon_name, width=25, style={"margin-right": "5px"})
+            if icon_name != None
+            else None
+        )
+
+        buttons_list.append(
+            dbc.Button(
+                children=[icon, button_text],
+                outline=True,
+                color="primary",
+                style={"display": "flex", "align-items": "start"},
+                id=button_id,
+            )
+        )
+    return dbc.ButtonGroup(buttons_list, vertical=True)
+
+
+def audio_leftcollumn(source):
+    if source != "col" and source != "drawer":
+        raise ValueError
+
+    content = dmc.Stack(
+        [
+            get_buttongroup_with_icons(
+                [
+                    ["Главная", "home", "audioplayer_mainpage"],
+                    ["Поиск музыки", "search", "audioplayer_search"],
+                    ["Каталоги музыки", "catalogs", "audioplayer_catalogs"],
+                ]
+            ),
+            dmc.Divider(color="--bs-blue"),
+            html.H5("Медиатека"),
+            get_buttongroup_with_icons(
+                [
+                    ["Любимые треки", "playlist", "id_1"],
+                    ["My favourite rock", "playlist", "id_2"],
+                    ["AC/DC", "artist", "id_3"],
+                    ["Rammstein", "artist", "id_4"],
+                    ["Рок", "genre", "id_5"],
+                    ["Хаус", "genre", "id_6"],
+                ]
+            ),
+        ]
+    )
+
+    return content
+
+
+def get_drawer():
+    return dmc.Drawer(
+        children=[audio_leftcollumn(source="drawer")],
+        title=html.H5("Аудиоплеер"),
+        id="drawer-albums",
+        padding="md",
+        zIndex=10000,
+    )
+
+
+def float_player():
+    return html.Div(
+        [
+            dmc.Grid(
+                [
+                    dmc.Col(
+                        [
+                            dmc.ActionIcon(
+                                DashIconify(
+                                    icon="iconamoon:menu-burger-horizontal",
+                                    width=35,
+                                    color="var(--bs-primary)",
+                                ),
+                                size="50px",
+                                radius="md",
+                                variant="default",
+                                id="open-drawer-albums",
+                                className="shown_affix",
+                            ),
+                        ],
+                        span="content",
+                    ),
+                    dmc.Col(
+                        dmc.Stack(
+                            [
+                                html.P(
+                                    "Название песни",
+                                    className="text-primary",
+                                    style={"margin-bottom": 0},
+                                    id="song-name",
+                                ),
+                                html.P(
+                                    "Исполнитель",
+                                    className="text-secondary",
+                                    style={"margin-bottom": 0},
+                                    id="song-artist",
+                                ),
+                            ],
+                            spacing=0,
+                        ),
+                        span="content",
+                    ),
+                    dmc.Col(span="auto"),
+                    dmc.Col(
+                        [
+                            dmc.Slider(
+                                min=0,
+                                max=100,
+                                id="volume-slider",
+                                value=33,
+                                w='120px',
+                                color="gray.7",
+                                thumbLabel='Громкость'
+                            ),
+                        ],
+                        span="content",
+                        style={"align-items": "center", "display": "flex"},
+                    ),
+                ]
+            )
+        ],
+        style={
+            "min-height": "70px",
+        },
+        className="block-background",
+    )
--- pages/players/audioplayer.py
+++ pages/players/audioplayer.py
@@ -13,64 +13,78 @@ from dash import (
 import dash_mantine_components as dmc
 import dash_bootstrap_components as dbc
 from dash_extensions import Purify
+from dash_iconify import DashIconify
 from flask import request
-from datetime import datetime 
+from datetime import datetime
 import pandas as pd
+from controllers import cont_audioplayer as cont_a
 
 register_page(__name__, path="/players/audioplayer", icon="fa-solid:home")
 
-def get_playlist_button(name, href='https://example.com', content_type='Плейлист'):
-    return html.A(dmc.Group([name, dbc.Badge(content_type, color='primary')]), href=href, style={'text-decoration': 'none'})
 
-def create_table(df):
-    columns, values = df.columns, df.values
-    header = [html.Tr([html.Th(col) for col in columns])]
-    rows = [html.Tr([html.Td(cell) for cell in row]) for row in values]
-    table = [html.Thead(header), html.Tbody(rows)]
-    return table
-
-def layout(l = 'n', **kwargs):
+def layout(l="n", selected_playlist_name='Любимые треки', **kwargs):
     # lazy load block
-    if l == 'n':
+    if l == "n":
         return dmc.Container()
     else:
-        now = datetime.now().strftime("%d/%b/%Y %H:%M:%S") 
-        print(f'{request.remote_addr} - - [{now}] | audioplayer')
-        df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-
-    return dmc.Container(
-        children=[
-            dbc.Row(children=[
-                dbc.Col(lg=3, md=4, children=[
-                    html.Div(style={'height': '100%'}, className='block-background-audio', children=[
-                        dmc.Stack([
-                            dbc.Button('Главная', outline=True, color='primary'),
-                            dbc.Button('Поиск музыки', outline=True, color='primary'),
-                            dmc.Divider(color='--bs-blue'),
-                            get_playlist_button('Любимые треки'),
-                            get_playlist_button('My favourite rock'),
-                            get_playlist_button('AC/DC', content_type='Исполнитель'),
-                            get_playlist_button('Рок', content_type='Жанр'),
-                            get_playlist_button('Back in Black (AC/DC)', content_type='Альбом'),
-                        ])
-                    ]),
-                ], className="mrrow phone-col"),
-                dbc.Col(width=True, children=[
-                    html.Div(style={'height': '500px', 'padding': '1%'}, className='block-background-audio', children=[
-                        dmc.Stack([
-                            html.H4('Плейлист: Мои любимые треки'),
-                            dmc.Divider(color='--bs-blue'),
-                            html.Div(dmc.Table(create_table(df)), className='table-wrapper'),
-                        ])
-                    ]),
-                ], className="mrrow phone-col"),
+        now = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
+        print(f"{request.remote_addr} - - [{now}] | audioplayer page")
+        df = pd.read_csv(
+            "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+        )
+        return dmc.Container(
+            children=[
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                dmc.Stack(
+                                    [
+                                        cont_a.audio_leftcollumn(source="col"),
+                                    ],
+                                    className="block-background",
+                                )
+                            ],
+                            className="hided_column",
+                            width=3,
+                        ),
+                        dbc.Col(
+                            [
+                                html.Div(
+                                    style={"height": "500px"},
+                                    className="block-background-audio",
+                                    children=[
+                                        dmc.Stack(
+                                            [
+                                                html.H4(selected_playlist_name, style={'margin-bottom': '0 !important'}),
+                                                dmc.Divider(color="--bs-blue"),
+                                                html.Div(
+                                                    dmc.Table(cont_a.create_table(df)),
+                                                    className="table-wrapper",
+                                                ),
+                                            ]
+                                        )
+                                    ],
+                                )
+                            ]
+                        ),
+                    ]
+                ),
+                dmc.Affix(
+                    children=[cont_a.float_player()],
+                    style={'width': '100%'},
+                ),
+                cont_a.get_drawer(),
             ],
-            className="g-0",
-            # style={'height': '500px'}
-            )
-        ],
-        pt=20,
-        # style={"paddingTop": 20},
-        className='dmc-container',
-        size="100%",
-    )
+            pt=20,
+            className="dmc-container adaptive-container",
+        )
+
+
+@callback(
+    Output("drawer-albums", "opened"),
+    Input("open-drawer-albums", "n_clicks"),
+    prevent_initial_call=True,
+)
+def drawer_with_albums(n_clicks):
+    return True
